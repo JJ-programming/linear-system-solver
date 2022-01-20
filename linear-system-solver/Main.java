497b0cebc87e392a559c644364fed1cc6e0216ef
Yosif Taibi <yosif.taibi@gmail.com>
2022-01-20 22:07:30 +0200

    nothing special

--- linear-system-solver/Main.java
+++ linear-system-solver/Main.java
@@ -7,28 +7,34 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner input=new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
         System.out.print("Enter the variables count:");
-        int variables=input.nextInt();
+        int variables = input.nextInt();
         System.out.println(variables);
 
-        Matrix matrix=new Matrix(variables);
+        Matrix matrix = new Matrix(variables);
 
-        for(int row=0;row<variables;row++){
-            double currentRowCoefficients[]=new double[variables+1];
-            for(int coefficient=0;coefficient<variables+1;coefficient++){
-                if(coefficient!=variables){
-                    System.out.print("Enter the coefficient for the variable '"+(char)('a'+coefficient)+"':");
+        for(int row = 0; row<variables ; row++){
+            double[] currentRowCoefficients = new double[variables+1];
+
+            for(int coefficient = 0; coefficient<variables+1 ; coefficient++){
+                if(coefficient != variables){
+                    System.out.print("Enter the coefficient for the variable '" + (char)('a'+coefficient)+"':");
                 }else{
                     System.out.print("Enter the value of the expression:");
                 }
-                currentRowCoefficients[coefficient]=Double.parseDouble(input.next());//Supports both int and double inputs
+
+                currentRowCoefficients[coefficient] = Double.parseDouble(input.next());//Supports both int and double inputs
             }
             matrix.setRow(row,currentRowCoefficients);
         }
 
+        System.out.println("\n\nExpression:");
         matrix.printAsExpression();
+
+        System.out.println("\n\nMatrix:");
+        matrix.print();
 
         matrix.solveMatrix();
     }
--- linear-system-solver/Matrix.java
+++ linear-system-solver/Matrix.java
@@ -3,82 +3,51 @@ package com.company;
 import java.util.Arrays;
 
 public class Matrix{
-    private int rows;
-    private int columns;
-    private double elements[][];
+    private final int rows;
+    private final int columns;
+    private final double[][] elements;
 
     public Matrix(int variablesCount){
-        this.rows=variablesCount;
-        this.columns=variablesCount+1;//Adding an extra column for the answer of each expression (row)
-
-
-        this.elements=new double[this.rows][this.columns];
-
-        
-    }
-
-    public Matrix(Matrix targetMatrix){//Copy constructor
-        this.rows=targetMatrix.rows;
-        this.columns=targetMatrix.columns;
-
-        this.elements=new double[this.rows][this.columns];
-
-        for(int row=0;row<targetMatrix.rows;row++){
-            for(int column=0;column<targetMatrix.columns;column++){
-                this.elements[row][column]=targetMatrix.elements[row][column];
-            }
-        }
+        this.rows = variablesCount;
+        this.columns = variablesCount + 1;//Adding an extra column for the answer of each expression (row)
+        this.elements = new double[this.rows][this.columns];
     }
 
     ////////////////////////////////////////////////Matrix operations
-    public double[] getMultipliedRow(int row,double multValue){
-        double resultRow[]=Arrays.copyOf(this.elements[row],this.elements[row].length);
+    public double[] getMultipliedRow (int rowIndex , double multiplyBy) {
+        double[] resultRow = Arrays.copyOf(this.elements[rowIndex], this.elements[rowIndex].length);
 
-        for(int i=0;i<resultRow.length;i++){
-            resultRow[i]*=multValue;
+        for(int i = 0 ; i<resultRow.length ; i++){
+            resultRow[i] *= multiplyBy;
         }
 
         return resultRow;
     }
 
-    public void swapRows(int row1,int row2){
-        double row1Copy[]=Arrays.copyOf(this.getRow(row1),this.getRow(row1).length);
+    public void swapRows (int row1 , int row2) {
+        double[] row1Copy = Arrays.copyOf(this.getRow(row1), this.getRow(row1).length);
 
-        this.setRow(row1,Arrays.copyOf(this.getRow(row2),this.getRow(row2).length));
+        this.setRow(row1, Arrays.copyOf(this.getRow(row2), this.getRow(row2).length));
 
         this.setRow(row2,row1Copy);
     }
 
-    public void multiplyRow(int row,double multValue){
-        this.setRow(row,getMultipliedRow(row,multValue));
+    public void multiplyRow(int rowIndex , double multiplyBy){
+        this.setRow(rowIndex,getMultipliedRow(rowIndex,multiplyBy));
     }
 
-    public double[] getDividedRow(int row,double divisionValue){
-        return getMultipliedRow(row,1/divisionValue);
+    public double[] getDividedRow(int rowIndex , double divideBy){
+        return getMultipliedRow(rowIndex,1/divideBy);
     }
 
-    public void divideRow(int row,double divisionValue){
-        this.setRow(row,getDividedRow(row,divisionValue));
+    public void divideRow(int rowIndex , double divideBy){
+        this.setRow(rowIndex,getDividedRow(rowIndex,divideBy));
     }
 
-    public void addRows(int row1,double row2[]){//Adds the second parameter row to the first
-        for(int i=0;i<elements[row1].length;i++){
-            this.elements[row1][i]+=row2[i];
+    public void subtractRows(int row1,double[] row2){
+        for(int i = 0 ; i<elements[row1].length ; i++){
+            this.elements[row1][i] -= row2[i];
         }
-    }
-
-    public void addRows(int row1,int row2){
-        this.addRows(row1,this.elements[row2]);
-    }
-
-    public void subtractRows(int row1,double row2[]){
-        for(int i=0;i<elements[row1].length;i++){
-            this.elements[row1][i]-=row2[i];
-        }
-    }
-
-    public void subtractRows(int row1,int row2){//Adds the second parameter row to the first
-        subtractRows(row1,this.elements[row2]);
     }
 
     //Goes column by column and makes each element of the matrix except it's diagonal equal to 0
@@ -128,31 +97,28 @@ public class Matrix{
             }
         }
 
-        System.out.println("Answers to the system:");
+        System.out.println("Solution of the system:");
         for(int row=0;row<this.rows;row++){
             this.divideRow(row,this.elements[row][row]);
             this.elements[row][row]=Math.round(this.elements[row][row]);
-
-            System.out.println((char)('a'+row)+" = "+this.elements[row][this.columns-1]);
+            if (Double.isNaN(this.elements[row][this.columns-1])){
+                System.out.println("The system has no solution!");
+                return;
+            }else{
+                System.out.println((char)('a'+row)+" = "+this.elements[row][this.columns-1]);
+            }
         }
     }
     ////////////////////////////////////////////////
-
-    public boolean isVariable(char character){
-        return (character>='a' && character<='z');
-    }
 
     public double[] getRow(int index){
         return this.elements[index];
     }
 
-    public void setRow(int rowIndex,double row[]){
-        this.elements[rowIndex]=row;
+    public void setRow(int rowIndex , double[] row){
+        this.elements[rowIndex] = row;
     }
 
-    public double getElement(int row,int column){
-        return this.elements[row][column];
-    }
 
     public void print(){
         for(int row=0;row<this.rows;row++){
